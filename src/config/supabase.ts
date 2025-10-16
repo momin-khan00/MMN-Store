be90faa807ed80c3792d025b5cde0b69d53a223f
momin-khan00 <dangerkhan598@gmail.com>
2025-10-16 10:35:20 +0530

    Update supabase.ts

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+// The '!' tells TypeScript that we are sure these values will not be null.
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
