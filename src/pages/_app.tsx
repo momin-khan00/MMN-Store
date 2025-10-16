1e9db83604f5301ee9d3600eedbcf90676e3b1bc
momin-khan00 <dangerkhan598@gmail.com>
2025-10-16 17:20:03 +0530

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,17 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AuthProvider } from '@/context/AuthContext'
 import Header from '@/components/common/Header'
+import Footer from '@/components/common/Footer' // Import Footer
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
-      <div className="bg-gray-900 text-gray-100 min-h-screen">
+      <div className="flex flex-col min-h-screen bg-dark-900 text-gray-100">
         <Header />
-        <main>
+        <main className="flex-grow">
           <Component {...pageProps} />
         </main>
+        <Footer />
       </div>
     </AuthProvider>
   )
