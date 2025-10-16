bd6f1b6ca30eaae4099a62e4b5b9a436cea63808
momin-khan00 <dangerkhan598@gmail.com>
2025-10-16 09:56:03 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    domains: ['api.backblazeb2.com', 'f002.backblazeb2.com', 'https://vioqetdvaoxzrqcuuclu.supabase.co'],
+  },
+  env: {
+    BACKBLAZE_BUCKET_URL: process.env.BACKBLAZE_BUCKET_URL,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
