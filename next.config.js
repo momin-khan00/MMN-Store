fcb004e9b342fd3a5f2ffa040edba75b7fa18fb1
momin-khan00 <dangerkhan598@gmail.com>
2025-10-16 09:41:43 +0530

    Clean up next.config.js by removing unused settings
    Removed image domains, environment variables, TypeScript, and ESLint configurations from next.config.js.

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  images: {
-    domains: ['api.backblazeb2.com', 'f002.backblazeb2.com', 'https://vioqetdvaoxzrqcuuclu.supabase.co'],
-  },
-  env: {
-    BACKBLAZE_BUCKET_URL: process.env.BACKBLAZE_BUCKET_URL,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  // Static export नहीं करें
-  // output: 'export', // इस line को remove करें या comment करें
 };
 
 module.exports = nextConfig;
