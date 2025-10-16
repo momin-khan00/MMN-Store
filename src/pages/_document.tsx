e2b4dd100a3b742c76e9941bcf54185ad465b256
momin-khan00 <dangerkhan598@gmail.com>
2025-10-16 09:59:38 +0530

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,14 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta charSet="utf-8" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
-        <div id="modal-root"></div>
       </body>
     </Html>
   );
