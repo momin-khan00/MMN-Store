5c514a82dad0d30db589ff3382ed9a41d62c9d84
momin-khan00 <dangerkhan598@gmail.com>
2025-10-16 08:51:53 +0530

    Add viewport and charset meta tags to Document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,10 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charSet="utf-8" />
+      </Head>
       <body>
         <Main />
         <NextScript />
+        <div id="modal-root"></div>
       </body>
     </Html>
   );
