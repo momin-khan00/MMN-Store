5ec64fc5d3e2cc03e9de1c51bb6c2e6de1af1711
momin-khan00 <dangerkhan598@gmail.com>
2025-10-16 14:46:35 +0530

    Update Loading.tsx

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -1,7 +1,9 @@
 export default function Loading() {
   return (
-    <div className="flex justify-center items-center p-10">
-      <div className="w-12 h-12 border-4 border-dashed rounded-full animate-spin border-brand-light"></div>
+    <div className="flex justify-center items-center py-10">
+      <div className="w-12 h-12 border-4 border-dashed rounded-full animate-spin border-brand-light" role="status">
+        <span className="sr-only">Loading...</span>
+      </div>
     </div>
   );
 }
