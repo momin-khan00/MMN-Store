491addfdb51109a915016d9a44d078c5c52a1f8d
momin-khan00 <dangerkhan598@gmail.com>
2025-10-16 10:38:29 +0530

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,19 @@
+import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { AuthProvider } from '../context/AuthContext';
-import '../styles/globals.css';
+import Header from '../components/common/Header';
+import Footer from '../components/common/Footer';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
-      <Component {...pageProps} />
+      <div className="flex flex-col min-h-screen bg-gray-900 text-gray-100">
+        <Header />
+        <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-8">
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+      </div>
     </AuthProvider>
   );
 }
