285cb9e856c6001fcafde94ac55a23ef11f41a5f
momin-khan00 <dangerkhan598@gmail.com>
2025-10-18 13:50:05 +0530

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,18 +2,22 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AuthProvider } from '@/context/AuthContext'
 import Header from '@/components/common/Header'
-import Footer from '@/components/common/Footer' // Import Footer
+import Footer from '@/components/common/Footer'
+import { ThemeProvider } from 'next-themes' // Import ThemeProvider
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
-      <div className="flex flex-col min-h-screen bg-dark-900 text-gray-100">
-        <Header />
-        <main className="flex-grow">
-          <Component {...pageProps} />
-        </main>
-        <Footer />
-      </div>
-    </AuthProvider>
+    <ThemeProvider attribute="class" defaultTheme="system">
+      <AuthProvider>
+        {/* THE FIX: Added styles for light/dark mode */}
+        <div className="flex flex-col min-h-screen bg-gray-100 dark:bg-dark-900 text-gray-800 dark:text-gray-100 transition-colors duration-300">
+          <Header />
+          <main className="flex-grow">
+            <Component {...pageProps} />
+          </main>
+          <Footer />
+        </div>
+      </AuthProvider>
+    </ThemeProvider>
   )
 }
